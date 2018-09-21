3af255d1567f433e19b4efe03bc1359eeb94b63c
ferencDobi <ferencdbi@gmail.com>
2018-09-22 00:31:39 +0200

    highest card refactor to use streams

--- src/PokerClient.java
+++ src/PokerClient.java
@@ -11,27 +11,9 @@ public class PokerClient {
     }
 
     public boolean highestCardIsMine(String p1, String p2, String p3, String p4, String p5) {
-        Card highestCard = new Card("s2");
-        List<Card> opposingHand = Stream.of(p1, p2, p3, p4, p5).map(Card::new).collect(Collectors.toList());
+        int opposingHighest = Stream.of(p1, p2, p3, p4, p5).map(Card::new).mapToInt(Card::getValue).max().getAsInt();
 
-
-        for (int i = 0; i < opposingHand.size(); i++) {
-            Card ownCard = hand.get(i);
-            for (int j = 0; j < opposingHand.size(); j++) {
-                Card opposingCard = opposingHand.get(j);
-                if (opposingCard.getValue() >= ownCard.getValue()) {
-                    if (opposingCard.getValue() >= highestCard.getValue()) {
-                        highestCard = opposingCard;
-                    }
-                } else {
-                    if (ownCard.getValue() > highestCard.getValue()) {
-                        highestCard = ownCard;
-                    }
-                }
-            }
-        }
-
-        return hand.contains(highestCard);
+        return hand.stream().mapToInt(Card::getValue).anyMatch(card -> card > opposingHighest);
     }
 
 }
